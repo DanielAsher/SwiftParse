8ff934312ec3cd8a561e2652f1d6426b9a4b8715
Daniel Asher <daniel.asher.2303@gmail.com>
2015-09-11 05:23:05 +0100

    add .travis.yml, SwiftParse-OSX target and share Schemes

---
+++ SwiftParse/SwiftParse-OSX/SwiftParse-OSX.h
@@ -0,0 +1,19 @@
+//
+//  SwiftParse-OSX.h
+//  SwiftParse-OSX
+//
+//  Created by Daniel Asher on 11/09/2015.
+//  Copyright © 2015 StoryShare. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+//! Project version number for SwiftParse-OSX.
+FOUNDATION_EXPORT double SwiftParse_OSXVersionNumber;
+
+//! Project version string for SwiftParse-OSX.
+FOUNDATION_EXPORT const unsigned char SwiftParse_OSXVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <SwiftParse_OSX/PublicHeader.h>
+
+
---
+++ SwiftParse/SwiftParse-OSXTests/SwiftParse_OSXTests.swift
@@ -0,0 +1,36 @@
+//
+//  SwiftParse_OSXTests.swift
+//  SwiftParse-OSXTests
+//
+//  Created by Daniel Asher on 11/09/2015.
+//  Copyright © 2015 StoryShare. All rights reserved.
+//
+
+import XCTest
+@testable import SwiftParse_OSX
+
+class SwiftParse_OSXTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
